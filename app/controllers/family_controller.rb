67797d6c56a6c48898298fe63faa15153a657550
George Baum <george@georgebaum.me.uk>
2026-05-26 16:20:58 +1000

    yet another add student commit

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -1,4 +1,5 @@
 class FamilyController < ApplicationController
   def index
+    @user = current_user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_26_010715) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_26_070000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "students", force: :cascade do |t|
     t.integer "age"
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "first_name"
+    t.integer "school_year"
+    t.string "state"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_students_on_user_id"
