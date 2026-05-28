6a5d1ffd4d452e64ccc08d1a16cb2cb138b30922
George Baum <george@georgebaum.me.uk>
2026-05-28 12:22:08 +1000

    working next and previous buttons

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,13 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @practice = @question.practice
-    @position = @practice.questions.to_a.index(@question) + 1
+
+    questions = @practice.questions.order(:id).to_a
+
+    index = questions.index(@question)
+
+    @position = index + 1
+    @next_question = questions[index + 1]
+    @previous_question = index.positive? ? questions[index - 1] : nil
   end
 end
