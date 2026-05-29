46f598512057f8692c593f83cc04148be11270ce
Halo Arieli <hila.m.arieli@gmail.com>
2026-05-29 13:01:40 +1000

    lesson controller and more routes

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,22 @@
 class LessonsController < ApplicationController
   def show
+    @lesson = @practice.lesson
   end
 
   def create
+    @lesson = @practice.build_lesson
+    # AI generation will go here later
+    @lesson.content = "Lesson coming soon..."
+    if @lesson.save
+      redirect_to practice_lesson_path(@practice)
+    else
+      redirect_to practice_path(@practice), alert: "Could not generate lesson"
+    end
+  end
+
+  private
+
+  def set_practice
+    @practice = Practice.find(params[:practice_id])
   end
 end
