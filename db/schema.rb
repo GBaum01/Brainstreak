4d2b0ceb4a30a40b7986e1e27d80a5f38fe35061
George Baum <george@georgebaum.me.uk>
2026-05-29 12:21:08 +1000

    recap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_28_082628) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_29_093000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_28_082628) do
     t.bigint "practice_id", null: false
     t.text "question"
     t.boolean "status"
+    t.text "submitted_answer"
     t.datetime "updated_at", null: false
     t.index ["practice_id"], name: "index_questions_on_practice_id"
   end
